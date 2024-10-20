4acfb2133e46f74196a15ab903a367c37cd0153c
ykijel <ykijel@ucsc.edu>
2024-10-20 13:27:34 -0700

    Step 1: Initial non-interactive UI

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,11 @@ const APP_NAME = "Yahli's Game";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+app.innerHTML = `<h1>${APP_NAME}</h1>`;
+
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+canvas.id = "gameCanvas";
+
+app.appendChild(canvas);
